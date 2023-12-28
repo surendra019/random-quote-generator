eac2c9381b8df631e6bce0fc84aa4e3558005dcf
surendra019 <iamsurendra019@gmail.com>
2023-12-28 15:02:39 +0530

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+var quotes = require("quote-library");
+var express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
+
+app.get('/givequote', (req, res)=>{
+  
+res.json(`${JSON.stringify(quotes.randomQuote())}`);
+  console.log(
+JSON.stringify(quotes.randomQuote()));
+});
+
+app.get('/', (req, res)=>{
+res.render('main');
+})
+
+
+
+app.listen(5838);
